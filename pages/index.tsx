d0091480c1bb285b4602ab9f1d1bfb3170937526
mameli <filippo.mameli@dbiz.it>
2022-01-06 17:03:42 +0100

    init tailwind, next

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const IndexPage = () => (
+  <Layout title="Mameli.dev">
+    <h1>Hello 👋</h1>
+  </Layout>
+)
+
+export default IndexPage
