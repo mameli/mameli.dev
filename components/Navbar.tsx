292cefcbe4772a0723bfd27c3bb69f1c28833f31
mameli <filippo.mameli@dbiz.it>
2022-01-07 21:09:24 +0100

    fix nav items position

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -100,14 +100,14 @@ const Navbar = () => {
                   <a
                     key="logo"
                     href="/"
-                    className="text-xl text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'px-3 py-2 rounded-md text-sm font-medium"
+                    className="text-2xl text-gray-300',
+                    'rounded-md font-medium"
                   >
                     Mameli.Dev
                   </a>
                 </div>
-                <div className="hidden sm:block sm:ml-6">
-                  <div className="flex space-x-4">
+                <div className="hidden sm:block sm:ml-auto items-center">
+                  <div className="flex space-x-4 items-center">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
@@ -115,8 +115,8 @@ const Navbar = () => {
                         className={classNames(
                           item.current
                             ? 'bg-gray-900 text-white'
-                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'px-3 py-2 rounded-md text-sm font-medium'
+                            : 'dark:text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'px-3 py-10 rounded-md text-lg font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
                       >
@@ -124,9 +124,79 @@ const Navbar = () => {
                       </a>
                     ))}
                     <button
+                      aria-label="Toggle Dark Mode"
+                      type="button"
+                      className="w-10 h-10 rounded-lg flex items-center justify-center  hover:ring-2 ring-gray-900 dark:ring-gray-300  transition-all"
+                      onClick={() =>
+                        setTheme(theme === 'dark' ? 'light' : 'dark')
+                      }
+                    >
+                      {mounted && (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          className="w-5 h-5 text-gray-800 dark:text-gray-200"
+                        >
+                          {theme === 'dark' ? (
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                            />
+                          ) : (
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                            />
+                          )}
+                        </svg>
+                      )}
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 sm:hidden">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? 'bg-gray-900 text-white'
+                      : 'dark:text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-lg font-medium dark:text-white text-right'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+            <div className="px-2">
+              <button
                 aria-label="Toggle Dark Mode"
                 type="button"
-                className="w-5 h-5 rounded-lg flex items-center justify-center  hover:ring-2 ring-gray-900  transition-all"
+                className="mx-1 py-2 w-10 h-10 rounded-lg flex items-center justify-center  hover:ring-2 ring-gray-900  dark:ring-gray-300 transition-all ml-auto"
                 onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
               >
                 {mounted && (
@@ -155,41 +225,6 @@ const Navbar = () => {
                   </svg>
                 )}
               </button>
-                  </div>
-                  
-                </div>
-              </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 sm:hidden">
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-            </div>
-          </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? 'bg-gray-900 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block px-3 py-2 rounded-md text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
             </div>
           </Disclosure.Panel>
         </>
