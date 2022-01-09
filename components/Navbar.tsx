60dfcffb7b37b8c07d51502a5452c95d997fe6f0
mameli <filippo.mameli@dbiz.it>
2022-01-09 16:51:06 +0100

    update navbar style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,73 +11,6 @@ const Navbar = () => {
   const { theme, setTheme } = useTheme()
   const [mounted, setMounted] = useState(false)
 
-  const oldNav = (
-    <nav className="h-10 bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-      <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="#" className="flex">
-          <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">
-            Mameli
-          </span>
-        </a>
-        <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1">
-          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-            <li>
-              <Link href="/">
-                <a
-                  className="block py-2 pr-4 pl-3 text-white bg-green-700 rounded md:bg-transparent md:text-black md:p-0 dark:text-white"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/about">
-                <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  About
-                </a>
-              </Link>
-            </li>
-            <li>
-              <button
-                aria-label="Toggle Dark Mode"
-                type="button"
-                className="w-5 h-5 rounded-lg flex items-center justify-center  hover:ring-2 ring-gray-900  transition-all"
-                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-              >
-                {mounted && (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    className="w-5 h-5 text-gray-800 dark:text-gray-200"
-                  >
-                    {theme === 'dark' ? (
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                      />
-                    ) : (
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                      />
-                    )}
-                  </svg>
-                )}
-              </button>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  )
-
   useEffect(() => setMounted(true), [])
 
   const navigation = [
@@ -94,30 +27,24 @@ const Navbar = () => {
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-16">
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex-shrink-0 flex items-center">
+            <div className="relative flex items-center justify-between h-14 ">
+              <div className="flex-1 flex justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-auto flex items-center ml-4">
                   <a
                     key="logo"
                     href="/"
-                    className="text-2xl text-gray-300',
-                    'rounded-md font-medium"
+                    className="text-2xl rounded-md font-medium"
                   >
                     Mameli.Dev
                   </a>
                 </div>
-                <div className="hidden sm:block sm:ml-auto items-center">
-                  <div className="flex space-x-4 items-center">
+                <div className="hidden sm:block">
+                  <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
-                        className={classNames(
-                          item.current
-                            ? 'bg-gray-900 text-white'
-                            : 'dark:text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'px-3 py-10 rounded-md text-lg font-medium'
-                        )}
+                        className="hover:underline decoration-wavy underline-offset-8 decoration-green-500 p-2 rounded-xl text-lg font-medium"
                         aria-current={item.current ? 'page' : undefined}
                       >
                         {item.name}
@@ -126,7 +53,7 @@ const Navbar = () => {
                     <button
                       aria-label="Toggle Dark Mode"
                       type="button"
-                      className="w-10 h-10 rounded-lg flex items-center justify-center  hover:ring-2 ring-gray-900 dark:ring-gray-300  transition-all"
+                      className="w-10 h-10 rounded-xl flex items-center justify-center hover:ring-2 ring-gray-900 dark:ring-gray-300 transition-all text-cyan-500 dark:text-yellow-500"
                       onClick={() =>
                         setTheme(theme === 'dark' ? 'light' : 'dark')
                       }
@@ -137,7 +64,7 @@ const Navbar = () => {
                           viewBox="0 0 24 24"
                           fill="none"
                           stroke="currentColor"
-                          className="w-5 h-5 text-gray-800 dark:text-gray-200"
+                          className="w-7 h-7"
                         >
                           {theme === 'dark' ? (
                             <path
@@ -161,12 +88,12 @@ const Navbar = () => {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 sm:hidden">
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded hover:ring-2 ring-gray-900 dark:ring-gray-300 ring-offset-4">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    <XIcon className="block h-7 w-7" aria-hidden="true" />
                   ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    <MenuIcon className="block h-7 w-7" aria-hidden="true" />
                   )}
                 </Disclosure.Button>
               </div>
@@ -180,12 +107,7 @@ const Navbar = () => {
                   key={item.name}
                   as="a"
                   href={item.href}
-                  className={classNames(
-                    item.current
-                      ? 'bg-gray-900 text-white'
-                      : 'dark:text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block px-3 py-2 rounded-md text-lg font-medium dark:text-white text-right'
-                  )}
+                  className="hover:underline decoration-wavy underline-offset-8 decoration-green-500 p-2 rounded-xl text-lg font-medium block px-2 py-2 dark:text-white text-right"
                   aria-current={item.current ? 'page' : undefined}
                 >
                   {item.name}
@@ -196,7 +118,7 @@ const Navbar = () => {
               <button
                 aria-label="Toggle Dark Mode"
                 type="button"
-                className="mx-1 py-2 w-10 h-10 rounded-lg flex items-center justify-center  hover:ring-2 ring-gray-900  dark:ring-gray-300 transition-all ml-auto"
+                className="w-10 h-10 rounded-lg flex items-center justify-center  hover:ring-2 ring-gray-900 dark:ring-gray-300 transition-all ml-auto "
                 onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
               >
                 {mounted && (
@@ -205,7 +127,7 @@ const Navbar = () => {
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
-                    className="w-5 h-5 text-gray-800 dark:text-gray-200"
+                    className="w-7 h-7 dark:text-gray-200 text-cyan-500 dark:text-yellow-500"
                   >
                     {theme === 'dark' ? (
                       <path
