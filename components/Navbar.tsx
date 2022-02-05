19944939a452cc7fa279a5ed7b68b952182bc54f
mameli <filippo.mameli@dbiz.it>
2022-02-05 15:57:17 +0100

    navbar links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,6 +13,9 @@ const Navbar = ({ layoutNavbar }) => {
 
   const navigation = [
     { name: 'Home', href: '/', current: false },
+    { name: 'Resources', href: '/resources', current: false },
+    { name: 'Setup', href: '/setup', current: false },
+    { name: 'Hobbies', href: '/hobbies', current: false },
     { name: 'About', href: '/about', current: false },
   ]
 
---
+++ pages/resources.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const ResourcesPage = () => (
+  <Layout title="About">
+    <div className="flex flex-row mx-auto">
+      <div className="flex ">
+        <h1 className="text-5xl text-gray-800 dark:text-white">About what?</h1>
+      </div>
+    </div>
+  </Layout>
+)
+
+export default ResourcesPage
