035a8a1335bbe50b435ed2562b75e121195be4e0
mameli <filippo.mameli@dbiz.it>
2022-04-17 17:10:26 +0200

    init resources page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,12 @@ const Footer = () => {
   return (
     <footer className="flex flex-col justify-center h-10">
       <hr />
-      <span className="m-auto">Created with ♥ by Mameli</span>
+      <span className="m-auto">Created with ♥ by <a
+            className="text-cyan-500 hover:underline underline-offset-2"
+            href="https://github.com/mameli"
+          >
+            Mameli
+          </a></span>
     </footer>
   )
 }
--- pages/resources.tsx
+++ pages/resources.tsx
@@ -9,6 +9,33 @@ const ResourcesPage = () => (
       </div>
       <div className="py-3"></div>
       <h2 className="text-3xl text-gray-800 dark:text-white">Under construction 🚧🚧🚧 ...</h2>
+      <p>
+      I like to keep a neat, categorized list of all the cool stuff I&apos;ve found online over the years.
+      I&apos;ll be the main consumer of this page, but I hope it can be useful to you as well.
+      </p>
+      <div className="py-3"></div>
+      <h2 className="text-2xl text-gray-800 dark:text-white">Development</h2>
+      <div className="py-3"></div>
+      <h2 className="text-xl text-gray-800 dark:text-white">News aggregator</h2>
+      <ul>
+        <li>
+          <a
+            className="text-cyan-500 hover:underline underline-offset-2"
+            href="https://news.ycombinator.com/"
+          >
+            Hacker News
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-cyan-500 hover:underline underline-offset-2"
+            href="https://lobste.rs/"
+          >
+            Lobste.rs
+          </a>
+        </li>
+        
+      </ul>
     </div>
   </Layout>
 )
