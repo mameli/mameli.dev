dd950ced9b2af8e755498d3bf6d7171ec0c951d1
Filippo Mameli <mameli93@gmail.com>
2025-12-24 02:52:32 +0100

    Astro pwa (#3)
    feat: init pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,56 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 import vercel from '@astrojs/vercel';
+import AstroPWA from '@vite-pwa/astro'
 
 import { SITE } from "./src/consts.ts";
 
 // https://astro.build/config
 export default defineConfig({
     site: SITE,
-    integrations: [mdx(), sitemap()],
+    integrations: [mdx(), sitemap(), AstroPWA({
+        mode: 'development',
+        base: '/',
+        scope: '/',
+        includeAssets: ['favicon.ico', 'favicon-180x180.png'],
+        manifest: {
+            name: 'Mameli Blog',
+            short_name: 'Mameli Blog',
+            theme_color: '#ffffff',
+            icons: [
+                {
+                    src: 'favicon-180x180.png',
+                    sizes: '180x180',
+                    type: 'image/png',
+                },
+                {
+                    src: 'favicon-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                },
+                {
+                    src: 'favicon-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                },
+                {
+                    src: 'favicon-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'any maskable',
+                },
+            ],
+        },
+        workbox: {
+            navigateFallback: '/',
+            globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+        },
+        devOptions: {
+            enabled: true,
+            navigateFallbackAllowlist: [/^\/$/],
+        },
+    }),
+    ],
 
     markdown: {
         shikiConfig: {
