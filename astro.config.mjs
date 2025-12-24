0cf9f515dbe36f2ec2a097bde376c580d24cfc04
Filippo Mameli <mameli93@gmail.com>
2025-12-24 01:04:36 +0100

    fix: add purpose

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,17 +23,20 @@ export default defineConfig({
                 {
                     src: 'assets/pwa-icon-192.png',
                     sizes: '192x192',
-                    type: 'image/png'
+                    type: 'image/png',
+                    purpose: 'any',
                 },
                 {
                     src: 'assets/pwa-icon-512.png',
                     sizes: '512x512',
-                    type: 'image/png'
+                    type: 'image/png',
+                    purpose: 'any',
                 },
                 {
                     src: 'assets/apple-touch-icon.png',
                     sizes: '180x180',
-                    type: 'image/png'
+                    type: 'image/png',
+                    purpose: 'any',
                 }
             ]
         }
